bb5b96183230663cb5e4d34f1eb702a25d73b034
thiago-ivasaki <thiagoivasaki@gmail.com>
2022-08-01 21:36:17 -0300

    Finalizado Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,46 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      disable: true,
+    };
+  }
+
+  Validation = () => {
+    const { name } = this.state;
+    const numb = 2;
+    const nameValidation = name.length < numb;
+    this.setState({ disable: nameValidation });
+  }
+
+  handleValue = (event) => {
+    this.setState({ name: event.target.value }, this.Validation);
+  }
+
   render() {
+    const { disable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            placeholder="Nome do Artísta"
+            data-testid="search-artist-input"
+            onChange={ this.handleValue }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ disable }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
