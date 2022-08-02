6b71084f5598c0cc679df2b10645855242798b23
thiago-ivasaki <thiagoivasaki@gmail.com>
2022-08-01 21:26:00 -0300

    Finalizado Requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -25,6 +26,9 @@ export default class Header extends React.Component {
     return (
       <header data-testid="header-component">
         <p data-testid="header-user-name">{ userName }</p>
+        <Link to="/search" data-testid="link-to-search">Busca</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+        <Link to="/profile" data-testid="link-to-profile">Conta</Link>
       </header>
     );
   }
